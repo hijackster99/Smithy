2229e0cbdea5abbaca645cd5cd243eeb31b0fa58
Jack Gagnon <jonathan.creed.gagnon@gmail.com>
2021-06-20 11:55:59 -0400

    initial commit

---
+++ src/main/java/com/hijackster99/core/References.java
@@ -0,0 +1,7 @@
+package com.hijackster99.core;
+
+public class References {
+
+	public static final String MODID = "smithy";
+	
+}
---
+++ src/main/java/com/hijackster99/core/Smithy.java
@@ -0,0 +1,10 @@
+package com.hijackster99.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(References.MODID)
+@Mod.EventBusSubscriber(modid = References.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Smithy {
+
+	
+}
